c5fb1eda76302abc312f95b7ad32c361be8d4507
Chandler Abril <abrilchandler1@gmail.com>
2024-01-23 18:45:21 -0700

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,8 @@
 import React from "react";
+// import {username, city} from "../Home.js"; don't need these because they're in Home
+import About from "./About.js"; //do we need this? yeah but you're not importing image. you should import About from ./About.js
+import NavBar from "./NavBar.js";
+import Home from "./Home.js";
 
 function App() {
   return (
